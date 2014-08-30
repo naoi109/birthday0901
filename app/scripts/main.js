7b421e5c85cc57381259a9afb1488135694e8f43
Naomi Isogai <citrusxtwins@gmail.com>
2014-08-30 16:48:41 +0900

    timer script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+'use strict'
+
+$(function(){
+	clock($("#timer"));
+
+	function clock(target){
+		var d = new Date();
+		var h = d.getHours();
+		var m = d.getMinutes();
+		var s = d.getSeconds();
+		h = h<10?"0"+h:h;
+		m = m<10?"0"+m:m;
+		s = s<10?"0"+s:s;
+		var time_str = h + ":" + m + ":" + s;
+		target.html(time_str);
+		if(s=="00"){
+			console.log("aaaa");
+		}
+		setTimeout(function(){
+			clock(target)
+		},1000);
+	}
+});
