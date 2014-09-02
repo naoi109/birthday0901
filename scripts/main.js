9f4130cd1aab3c2c8124b95aa04bf0f24fbf20a9
Naomi Isogai <citrusxtwins@gmail.com>
2014-09-03 01:22:05 +0900

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+$(function(){
+  //初期
+  clock($('#timer'));
+  jsonGet();
+});
+
+//時計
+function clock(target){
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  h = h<10?'0'+h:h;
+  m = m<10?'0'+m:m;
+  s = s<10?'0'+s:s;
+  var time_str = h + ':' + m + ':' + s;
+  target.html(time_str);
+  if(s=='00'||s=='30'){
+    jsonGet();
+  }
+  setTimeout(function(){
+    clock(target)
+  },1000);
+}
+
+//JSONからデータ取得
+function jsonGet(){
+  $.ajax({
+    type: 'GET',
+    url: 'scripts/data.json',
+    dataType: 'json',
+    success: function(json){
+      var data_num = Math.floor( Math.random() * 3 ); //データ個数
+      $('#place').html('<div>at '+json[data_num].place+'</div>');
+      $('#event').html('<div>'+json[data_num].date+' '+json[data_num].title+'</div>');
+      $('#bg').html('<img src="'+'images/photo/'+json[data_num].image_path+'.jpg">');
+      $('.bg img').hide();
+      $('.bg img').load(function(){
+        $('.bg img').show();
+        $('.bg').addClass('animate');
+      });
+    }
+  });
+}
+
+//preloder
+$.event.add(window, 'load', function() {
+    $('.event').addClass('animate');
+});
